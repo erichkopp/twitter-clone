88efb2fb5ac3d53c60836f55baf2b7ca5a9eed3d
Erich Kopp <erichkopp@gmail.com>
2018-08-20 19:12:39 -0400

    create show_user create following action create unfollowing action

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -4,19 +4,30 @@ class EpicenterController < ApplicationController
   def feed
   	@following_tweets = []
 
+  if current_user
   	Tweet.all.each do |tweet|
   		if current_user.following.include?(tweet.user.id) || current_user.id == tweet.user.id
   			@following_tweets.push(tweet)
   		end
   	end
   end
+end
 
   def show_user
+    @user = User.find(params[:id])
   end
 
   def now_following
+    current_user.following.push(params[:id].to_i)
+    current_user.save
+
+    redirect_to show_user_path(id: params[:id])
   end
 
   def unfollow
+    current_user.following.delete(params[:id].to_i)
+    current_user.save
+
+    redirect_to show_user_path(id: params[:id])
   end
 end
